a82f098aae32fb549c5f79fd75cfaeed4c1446c7
Anup shahi <bhanushahi70@gmail.com>
2025-08-05 16:17:45 +0530

    added some file requirement and procfile

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
+import os
 import string
 
 app = Flask(__name__)
@@ -96,3 +97,5 @@ def chat():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
